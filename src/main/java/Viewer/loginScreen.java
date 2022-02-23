6f3db935ecb102a4233d991d8075f4a0e15aa5ec
shehanikk <park.shehani.7@gmail.com>
2022-02-23 16:32:24 +0530

    login screen created

---
+++ src/main/java/Viewer/loginScreen.java
@@ -0,0 +1,35 @@
+package Viewer;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class loginScreen extends JFrame {
+    private JPanel lbLogin;
+    private JTextField tfUsername;
+    private JPasswordField tfPassword;
+    private JButton btnLogin;
+    private JLabel lbUsername;
+    private JLabel lbPassword;
+    private JFrame frame;
+
+    public loginScreen(){
+
+            frame = new JFrame("Login Frame");
+            frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            frame.setPreferredSize(new Dimension(300,300));
+            frame.setResizable(false);
+
+            frame.add(lbLogin);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+            setMinimumSize(new Dimension(450,450));
+
+
+    }
+
+    public static void main(String[] args) {
+        loginScreen form = new loginScreen();
+    }
+
+}
