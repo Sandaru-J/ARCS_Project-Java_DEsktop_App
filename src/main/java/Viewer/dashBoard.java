fc95ed01bc5781c1ee1af4798f72d823591cfd25
shehanikk <park.shehani.7@gmail.com>
2022-02-26 22:26:46 +0530

    dashBoard ui created

--- src/main/java/Viewer/dashBoard.java
+++ src/main/java/Viewer/dashBoard.java
@@ -1,4 +1,29 @@
 package Viewer;
 
+import javax.swing.*;
+
 public class dashBoard {
+    private JPanel dashPanel;
+    private JTabbedPane tabbedPane1;
+    private JTabbedPane tabbedPane2;
+    private JTabbedPane tabbedPane3;
+    private JTabbedPane tabbedPane4;
+
+
+    public dashBoard(){
+        JFrame frame = new JFrame("Dashboard");
+        frame.add(dashPanel);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(800,600);
+
+    }
+
+    public static void main(String[] args) {
+        dashBoard panel = new dashBoard();
+
+    }
+
+
 }
