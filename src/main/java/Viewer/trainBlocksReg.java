e2ac7ad4cddbc3b4f02a2a890a7cb05c8730dc14
Kalana <kalanabimsaradj5@gmail.com>
2022-02-26 11:03:21 +0530

    train block form designed

--- src/main/java/Viewer/trainBlocksReg.java
+++ src/main/java/Viewer/trainBlocksReg.java
@@ -1,4 +1,29 @@
 package Viewer;
 
+import javax.swing.*;
+
 public class trainBlocksReg {
+    private JPanel mainPanel;
+    private JPanel headerPanel;
+    private JButton registerButton;
+    private JButton cancelButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JComboBox comboBox1;
+    private JTextField textField3;
+    private JTextField textField6;
+
+    public trainBlocksReg(){
+        JFrame frame = new JFrame("Train Blocks Registration");
+        frame.add(mainPanel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        trainBlocksReg objTrainBlockReg = new trainBlocksReg();
+    }
 }
