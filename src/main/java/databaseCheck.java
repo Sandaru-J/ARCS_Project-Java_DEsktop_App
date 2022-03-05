3a6c2d89421dd350ee8feefc8c2fdf27cabde97c
Muditha Weerarathna <muditha@agrithmics.lk>
2022-03-05 09:12:11 +0530

    database connect check

--- src/main/java/databaseCheck.java
+++ src/main/java/databaseCheck.java
@@ -5,7 +5,7 @@ public class databaseCheck {
     public static void main(String args[]) {
 
       try{
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test1","root","mmw@sql");
         System.out.println("connect successfully");
 
