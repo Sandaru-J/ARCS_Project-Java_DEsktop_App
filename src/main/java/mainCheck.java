bba25d2ccd79201e2e362129ff8791966b443511
Muditha Weerarathna <muditha@agrithmics.lk>
2022-03-05 16:07:52 +0530

    Database connection done

---
+++ src/main/java/mainCheck.java
@@ -0,0 +1,15 @@
+import DatabaseConnection.MySqlConnection;
+import DatabaseConnection.SqlServerConnection;
+
+public class mainCheck {
+  public static void main(String [] args) {
+
+    //Mysql connection
+    MySqlConnection sqlConnection = new MySqlConnection();
+    sqlConnection.createConnectionMySql();
+
+    //sql server connection
+    SqlServerConnection sqlServerConnection = new SqlServerConnection();
+    sqlServerConnection.createConnectionSqlServer();
+  }
+}
