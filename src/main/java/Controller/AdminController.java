9ab9dfc132bd1592cb5318c800f0aff38e94e86f
Binuka Silva <76623940+binuka02@users.noreply.github.com>
2022-04-28 14:43:25 +0530

    Admin Signup

---
+++ src/main/java/Controller/AdminController.java
@@ -0,0 +1,22 @@
+package Controller;
+
+import Model.adminSignupModel;
+import Service.AdminService;
+
+public class AdminController {
+
+    public void adminSignup(String adminFullName, String adminUserName, String adminNIC, int adminContactNumber, String adminEmail, String adminPassword) {
+
+        adminSignupModel adminSignupModel = new adminSignupModel();
+
+        adminSignupModel.setAdminFullName(adminFullName);
+        adminSignupModel.setAdminUserName(adminUserName);
+        adminSignupModel.setAdminadminNIC(adminNIC);
+        adminSignupModel.setAdminContactNumber(adminContactNumber);
+        adminSignupModel.setAdminEmail(adminEmail);
+        adminSignupModel.setAdminPassword(adminPassword);
+
+        AdminService.adminSignup(adminSignupModel);
+    }
+
+}
