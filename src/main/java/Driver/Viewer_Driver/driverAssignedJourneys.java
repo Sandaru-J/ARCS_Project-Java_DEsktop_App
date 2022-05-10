fc56a5e7cfbf91493f48d9591950bd50f84df140
Binuka Silva <76623940+binuka02@users.noreply.github.com>
2022-05-11 02:08:13 +0530

    New

--- src/main/java/Driver/Viewer_Driver/driverAssignedJourneys.java
+++ src/main/java/Driver/Viewer_Driver/driverAssignedJourneys.java
@@ -11,7 +11,7 @@ import java.sql.*;
 
 public class driverAssignedJourneys {
 
-    public int JourneyID;
+    public String JourneyID;
     private JTable tblDriverAssignedJourneys;
     private JPanel panelDriverAssignedJourneys;
 
